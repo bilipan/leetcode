7b736e8c8a3f35657a6d93b9dd1885db7463996e
bili <panning20@gmail.com>
2021-02-24 07:25:54 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 module.exports = {
   twoSum: function (nums, target) {
-    const ret = [];
     for (let i = 0; i < nums.length - 1; i++) {
-      ret.push(i);
+      const ret = [];
       for (let j = i + 1; j < nums.length; j++) {
         if (nums[i] + nums[j] === target) {
+          ret.push(i);
           ret.push(j);
-          break;
+          return ret;
         }
       }
-      if (ret.length === 2) break;
-      ret.length = 0;
+    }
+  },
+  // 结果为倒序
+  twoSum2: function (nums, target) {
+    let map = new Map();
+    let ret = [];
+    for (let i = 0; i < nums.length; i++) {
+      if (map.has(nums[i])) {
+        ret["0"] = i;
+        ret["1"] = map.get(nums[i]);
+        return ret;
+      }
+      map.set(target - nums[i], i);
     }
     return ret;
   },
